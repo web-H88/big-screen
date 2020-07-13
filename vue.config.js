6ef117515dc50dce710d792afe569bdce262e84b
huangxu <huangxu@itcast.cn>
2020-07-13 17:51:58 +0800

    fix: 细节修复

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+
     productionSourceMap: false,
+
     lintOnSave: false,
     devServer: {
         port: 8080
     },
     configureWebpack: {
-        "jquery": "$",
-        "echarts": "echarts",
-        "axios": "axios"
+        externals: {
+            'jquery' : '$',
+            'echarts': 'echarts',
+            'axios' : 'axios'
+        }
     }
 }
